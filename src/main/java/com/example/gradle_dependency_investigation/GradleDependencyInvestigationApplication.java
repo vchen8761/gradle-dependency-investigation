7b2ecc9852029b6c8460499001d1aafb5165bfe5
cvictor <cvictor@vmware.com>
2022-01-11 11:53:51 -0500

    different ways to update transitive dependencies

---
+++ src/main/java/com/example/gradle_dependency_investigation/GradleDependencyInvestigationApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradle_dependency_investigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDependencyInvestigationApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(GradleDependencyInvestigationApplication.class, args);
+   }
+
+}
---
+++ src/test/java/com/example/gradle_dependency_investigation/GradleDependencyInvestigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradle_dependency_investigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDependencyInvestigationApplicationTests {
+
+   @Test
+   void contextLoads() {
+   }
+
+}
